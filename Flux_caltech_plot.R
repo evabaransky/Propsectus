7b83b63968ccfc5effd0e7ab92a6bc6d919894f1
evabaransky <ejb382@nau.edu>
2021-09-16 09:23:23 -0700

    Updating_caltech
    Doing caltech plot for fluxes on my computer. changed the destination.

--- Flux_caltech_plot.R
+++ Flux_caltech_plot.R
@@ -3,8 +3,8 @@ install.packages("tidyverse")
 library(ggplot2)
 library(tidyverse)
 
-setwd("Z:/Documents/GitHub/Prospectus")
-flux_dat<- read.csv("Flux_caltech_plot.csv")
+
+flux_dat<- read.csv("C:/Users/evaba/Documents/GitHub/Prospectus/Flux_caltech_plot.csv")
 limits=c(2.5*10^6,1.75*10^9)
 flux_dat %>% 
   ggplot(aes(label =Flux))+
@@ -14,10 +14,11 @@ flux_dat %>%
         axis.text.y=element_blank(),
         axis.ticks.y=element_blank(),
         legend.position = "none")+
-  scale_x_log10(limits=limits, name="Ni mol/yr" )+
+ # scale_x_log10(limits=limits, name="Ni mol/yr" )+
   annotate("text", x = 6*10^7, y=c(5.5,15.5), label = "?", size=6)+
-  geom_text(x = 9.5,y=(flux_dat$ymax+flux_dat$ymin)/2, size = 5, hjust = 0, aes(color = Flux))+
+#  geom_text(x = 9.5,y=(flux_dat$ymax+flux_dat$ymin)/2, size = 5, hjust = 0, aes(color = Flux))+
   theme(plot.margin = unit(c(1, 18, 1, 3), "lines")) +
-  geom_text(x=6.1, y= 15.5, label =  "outputs", angle = 90, size=5)+
-  geom_text(x=6.1, y= 5.5, label =  "inputs", angle = 90, size=5)+
+  xlim(c(-2*10^9,2*10^9))+
+  geom_text(x=3*10^9, y= 15.5, label =  "outputs", angle = 90, size=5)+
+  geom_text(x=-10^9, y= 5.5, label =  "inputs", angle = 90, size=5)+
   coord_cartesian(clip = "off")
